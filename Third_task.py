c6143e4f134dfed745b6420419106a8c952c1ecb
Pikh <matviy.pih1@gmail.com>
2026-05-03 14:33:56 +0300

    Therd_tasks that implement Principal Component Analysis

---
+++ Third_task.py
@@ -0,0 +1,76 @@
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.feature_engineering_new.ex4 import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.model_selection import cross_val_score
+from xgboost import XGBRegressor
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+
+
+def score_dataset(X, y, model=XGBRegressor()):
+    # Label encoding for categoricals
+    for colname in X.select_dtypes(["category", "object"]):
+        X[colname], _ = X[colname].factorize()
+    # Metric for Housing competition is RMSLE (Root Mean Squared Log Error)
+    score = cross_val_score(
+        model, X, y, cv=5, scoring="neg_mean_squared_log_error",
+    )
+    score = -1 * score.mean()
+    score = np.sqrt(score)
+    return score
+
+
+# Prepare data
+df = pd.read_csv("../input/fe-course-data/ames.csv")
+
+
+
+
+X = df.copy()
+y = X.pop("SalePrice")
+
+features = [
+    "LotArea",
+    "TotalBsmtSF",
+    "FirstFlrSF",
+    "SecondFlrSF",
+    "GrLivArea",
+]
+
+# Standardize
+X_scaled = X.loc[:, features]
+X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
+
+kmeans = KMeans(n_clusters=10, n_init=10, random_state=0)
+X["Cluster"] = kmeans.fit_predict(X_scaled)
+
+
+
+
+kmeans = KMeans(n_clusters=10, n_init=10, random_state=0)
+
+# YOUR CODE HERE: Create the cluster-distance features using `fit_transform`
+X_cd = kmeans.fit_transform(X_scaled)
+
+# Label features and join to dataset
+X_cd = pd.DataFrame(X_cd, columns=[f"Centroid_{i}" for i in range(X_cd.shape[1])])
+X = X.join(X_cd)
+
+
