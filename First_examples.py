1d21d91e0a707656dddc4757a04e7fe91b122e9b
Pikh <matviy.pih1@gmail.com>
2026-05-03 13:48:14 +0300

    feat: add reference examples and core concepts of feature engineering from course intro

---
+++ First_examples.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv("../input/fe-course-data/concrete.csv")
+df.head()
+
+
+X = df.copy()
+y = X.pop("CompressiveStrength")
+
+# Train and score baseline model
+baseline = RandomForestRegressor(criterion="absolute_error", random_state=0)
+baseline_score = cross_val_score(
+    baseline, X, y, cv=5, scoring="neg_mean_absolute_error"
+)
+baseline_score = -1 * baseline_score.mean()
+
+print(f"MAE Baseline Score: {baseline_score:.4}")
+
+X = df.copy()
+y = X.pop("CompressiveStrength")
+
+# Create synthetic features
+X["FCRatio"] = X["FineAggregate"] / X["CoarseAggregate"]
+X["AggCmtRatio"] = (X["CoarseAggregate"] + X["FineAggregate"]) / X["Cement"]
+X["WtrCmtRatio"] = X["Water"] / X["Cement"]
+
+# Train and score model on dataset with additional ratio features
+model = RandomForestRegressor(criterion="absolute_error", random_state=0)
+score = cross_val_score(
+    model, X, y, cv=5, scoring="neg_mean_absolute_error"
+)
+score = -1 * score.mean()
+
+print(f"MAE Score with Ratio Features: {score:.4}")
